73329c613df97a587db284fcb2b8cf4e7811055f
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-05 23:35:16 +0100

    Added IBotClient

---
+++ src/Thankies.Bot.Api/Client/IBotClient.cs
@@ -0,0 +1,10 @@
+﻿﻿using MihaZupan.TelegramBotClients;
+
+ namespace Thankies.Bot.Api.Client
+{
+    public interface IBotClient
+    {
+        RateLimitedTelegramBotClient Client { get; }
+        public string BotUrl { get; }
+    }
+}
