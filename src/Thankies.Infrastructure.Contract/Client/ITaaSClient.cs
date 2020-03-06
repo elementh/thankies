dc7c04baa1cb790045282205ce5cfc123bdccd7b
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-06 13:14:41 +0100

    Added infrastructure contracts

---
+++ src/Thankies.Infrastructure.Contract/Client/ITaaSClient.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Thankies.Infrastructure.Contract.Model;
+
+namespace Thankies.Infrastructure.Contract.Client
+{
+    public interface ITaaSClient
+    {
+        Task<GratitudeResponse> GetGratitude(string? name, string? filter, string language = "eng", CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/Thankies.Infrastructure.Contract/Service/IGratitudeService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thankies.Infrastructure.Contract.Service
+{
+    public interface IGratitudeService
+    {
+        Task<string?> Get(string? name, string? filter, string language = "eng", CancellationToken cancellationToken = default);
+    }
+}
