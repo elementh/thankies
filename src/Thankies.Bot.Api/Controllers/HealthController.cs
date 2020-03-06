519f7992db6095dabb908b865ff438fc6609df66
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-06 23:11:21 +0100

    Updated Health controller route

---
+++ src/Thankies.Bot.Api/Controllers/HealthController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Thankies.Bot.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        public readonly IConfiguration Configuration;
+
+        public HealthController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+        [HttpGet, Route("config")]
+        [Produces("application/json")]
+        public IActionResult GetConfigHealth()
+        {
+            var data = new
+            {
+                Basic = Configuration["Image:Basic"],
+                Mocking = Configuration["Images:Mocking"],
+                Shouting = Configuration["Images:Shouting"],
+                Leet = Configuration["Images:Leet"]
+            };
+
+            return Ok(data);
+        }
+    }
+}
