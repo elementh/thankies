6f4070e21a166fcbdbe2fa2f8aa2ef76f9125384
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-06 20:44:31 +0100

    Updated handler

--- src/Thankies.Core.Domain/ThanksInlineActionHandler.cs
+++ src/Thankies.Core.Domain/ThanksInlineActionHandler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot.Types.InlineQueryResults;
 using Thankies.Infrastructure.Contract.Service;
@@ -14,13 +15,23 @@ namespace Thankies.Core.Domain
     {
         protected readonly ILogger<ThanksInlineActionHandler> Logger;
         protected readonly IGratitudeService GratitudeService;
-
-        public ThanksInlineActionHandler(ILogger<ThanksInlineActionHandler> logger, IGratitudeService gratitudeService)
+        
+        protected static string Basic;
+        protected static string Mocking;
+        protected static string Shouting;
+        protected static string Leet;
+        
+        public ThanksInlineActionHandler(ILogger<ThanksInlineActionHandler> logger, IGratitudeService gratitudeService, IConfiguration configuration)
         {
             Logger = logger;
             GratitudeService = gratitudeService;
+            
+            Basic = configuration["Images.Basic"];
+            Mocking = configuration["Images.Mocking"];
+            Shouting = configuration["Images.Shouting"];
+            Leet = configuration["Images.Leet"];
         }
-
+        
         public async Task<IEnumerable<InlineQueryResultBase>> Handle(ThanksInlineAction request, CancellationToken cancellationToken)
         {
             var gratitude = await GratitudeService.GetForEveryFilter(request.Name, cancellationToken: cancellationToken);
@@ -29,12 +40,29 @@ namespace Thankies.Core.Domain
             {
                 throw new Exception();
             }
+
             return new List<InlineQueryResultBase>
             {
-                new InlineQueryResultArticle(gratitude[0].Item1, "Basic gratitude", new InputTextMessageContent(gratitude[0].Item2)),
-                new InlineQueryResultArticle(gratitude[1].Item1, "Mocking gratitude", new InputTextMessageContent(gratitude[1].Item2)),
-                new InlineQueryResultArticle(gratitude[2].Item1, "Shouting gratitude", new InputTextMessageContent(gratitude[2].Item2)),
-                new InlineQueryResultArticle(gratitude[3].Item1, "Leet gratitude", new InputTextMessageContent(gratitude[3].Item2))
+                new InlineQueryResultPhoto(nameof(Basic), Basic, Basic)  {
+                    Title = "Basic gratitude",
+                    Description = gratitude[0],
+                    InputMessageContent = new InputTextMessageContent(gratitude[0])
+                },
+                new InlineQueryResultPhoto(nameof(Basic), Basic, Basic)  {
+                    Title = "Mocking gratitude",
+                    Description = gratitude[0],
+                    InputMessageContent = new InputTextMessageContent(gratitude[1])
+                },
+                new InlineQueryResultPhoto(nameof(Basic), Basic, Basic)  {
+                    Title = "Shouting gratitude",
+                    Description = gratitude[0],
+                    InputMessageContent = new InputTextMessageContent(gratitude[2])
+                },
+                new InlineQueryResultPhoto(nameof(Basic), Basic, Basic)  {
+                    Title = "Leet gratitude",
+                    Description = gratitude[0],
+                    InputMessageContent = new InputTextMessageContent(gratitude[3])
+                }
             };
         }
     }
