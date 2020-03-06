802f7e67431d309be137b1359f724478cbf4c8ba
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-06 13:15:15 +0100

    Started with Inline action handler

---
+++ src/Thankies.Core.Domain/ThanksInlineAction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using Telegram.Bot.Types.InlineQueryResults;
+
+namespace Thankies.Core.Domain
+{
+    public class ThanksInlineAction : IRequest<IEnumerable<InlineQueryResultBase>>
+    {
+        
+    }
+}
---
+++ src/Thankies.Core.Domain/ThanksInlineActionHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot.Types.InlineQueryResults;
+using Thankies.Infrastructure.Contract.Service;
+
+namespace Thankies.Core.Domain
+{
+    public class ThanksInlineActionHandler : IRequestHandler<ThanksInlineAction, IEnumerable<InlineQueryResultBase>>
+    {
+        protected readonly ILogger<ThanksInlineActionHandler> Logger;
+        protected readonly IGratitudeService GratitudeService;
+
+        public ThanksInlineActionHandler(ILogger<ThanksInlineActionHandler> logger, IGratitudeService gratitudeService)
+        {
+            Logger = logger;
+            GratitudeService = gratitudeService;
+        }
+
+        public async Task<IEnumerable<InlineQueryResultBase>> Handle(ThanksInlineAction request, CancellationToken cancellationToken)
+        {
+            var gratitude = await GratitudeService.Get("James", "leet");
+
+            if (string.IsNullOrWhiteSpace(gratitude))
+            {
+                throw new Exception();
+            }
+            return new List<InlineQueryResultBase>()
+            {
+                new InlineQueryResultArticle("basic", "Basic gratitude", new InputTextMessageContent(gratitude)),
+                new InlineQueryResultArticle("mocking", "Mocking gratitude", new InputTextMessageContent(gratitude)),
+                new InlineQueryResultArticle("shouting", "Shouting gratitude", new InputTextMessageContent(gratitude)),
+                new InlineQueryResultArticle("leet", "Leet gratitude", new InputTextMessageContent(gratitude))
+            };
+        }
+    }
+}
