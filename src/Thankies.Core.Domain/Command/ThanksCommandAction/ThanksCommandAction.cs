f6ba2b8f1fa03f509055984031c57417c8b2f7ca
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-07 17:15:53 +0100

    Implemented thanks command action

--- src/Thankies.Core.Domain/Command/ThanksCommandAction/ThanksCommandAction.cs
+++ src/Thankies.Core.Domain/Command/ThanksCommandAction/ThanksCommandAction.cs
@@ -3,11 +3,29 @@ using Telegram.Bot.Types;
 
 namespace Thankies.Core.Domain.Command.ThanksCommandAction
 {
-    public class ThanksCommandAction : IRequest<string>
+    public class ThanksCommandAction : IRequest<(int?, string)>
     {
+        public readonly bool IsReply;
+        public readonly string? UserToReply;
+        public readonly int? MessageToReplyId;
+
         public ThanksCommandAction(Update update)
         {
-            
+            IsReply = update.Message.ReplyToMessage?.MessageId != null;
+
+            if (!IsReply)
+            {
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(update.Message.ReplyToMessage?.From?.Username))
+            {
+                UserToReply = update.Message.ReplyToMessage?.From?.Username;
+            }
+            else
+            {
+                MessageToReplyId = update.Message.ReplyToMessage.MessageId;
+            }
         }
     }
 }
---
+++ src/Thankies.Core.Domain/Command/ThanksCommandAction/ThanksCommandActionHandler.cs
@@ -0,0 +1,36 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Thankies.Infrastructure.Contract.Service;
+
+namespace Thankies.Core.Domain.Command.ThanksCommandAction
+{
+    public class ThanksCommandActionHandler : IRequestHandler<ThanksCommandAction, (int?, string)>
+    {
+        protected readonly ILogger<ThanksCommandActionHandler> Logger;
+        protected readonly IGratitudeService GratitudeService;
+
+        public ThanksCommandActionHandler(ILogger<ThanksCommandActionHandler> logger, IGratitudeService gratitudeService)
+        {
+            Logger = logger;
+            GratitudeService = gratitudeService;
+        }
+
+        public async Task<(int?, string)> Handle(ThanksCommandAction request, CancellationToken cancellationToken)
+        {
+            string gratitude;
+
+            if (!request.IsReply || string.IsNullOrWhiteSpace(request.UserToReply))
+            {
+                gratitude = await GratitudeService.Get(null, null, "eng", cancellationToken);
+            }
+            else
+            {
+                gratitude = await GratitudeService.Get(request.UserToReply, null, "eng", cancellationToken);
+            }
+
+            return (request.MessageToReplyId, gratitude);
+        }
+    }
+}
